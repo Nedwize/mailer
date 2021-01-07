ec39477abf414b75dd94e94dc453921b91df2cec
NakshatraCodes <nakshatra163@gmail.com>
2021-01-07 19:47:29 +0530

    Testing subsequent deployment

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: "there"});
+    res.send({bye: "Buddy we changed it"});
 })
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, (req, res) => {
-    console.log(`Server running on: http://localhost:${PORT}/`);
-})
+app.listen(PORT);
