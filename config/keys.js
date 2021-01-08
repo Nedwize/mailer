8d4e881f6ee56ad858457801f991ed2e93adbad1
NakshatraCodes <nakshatra163@gmail.com>
2021-01-08 22:50:23 +0530

    Debugging

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,5 @@ if(process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
     //We are in dev
-    module.exports = require('./prod');
+    module.exports = require('./dev');
 }
