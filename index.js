29900a670c73c3896cff53d2789698c0a8b8b9de
NakshatraCodes <nakshatra163@gmail.com>
2021-01-08 19:51:35 +0530

    Add Cookies

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 require('./models/User');
 require('./services/passport');
-(require('./routes/authRoutes'))(app);
 
 mongoose.connect(keys.mongoURI, {
         authSource: 'admin',
@@ -15,8 +16,15 @@ mongoose.connect(keys.mongoURI, {
         console.log("Connected to DB Servers");
     });
 
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
+(require('./routes/authRoutes'))(app);
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, ()=>{
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,12 @@ module.exports = (app) => {
     // Handles Redirect from Google Servers
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send({ message: "You were LOGGED OUT" });
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
 }
