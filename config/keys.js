48e6b654a62c20f7585329b6b65af2211393bcdc
NakshatraCodes <nakshatra163@gmail.com>
2021-01-08 21:24:23 +0530

    Test Production ENV Variables

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+
+if(process.env.NODE_ENV === 'production') {
+    //We are in production
+    module.exports = require('./prod');
+} else {
+    //We are in dev
+    module.exports = require('./prod');
+}
