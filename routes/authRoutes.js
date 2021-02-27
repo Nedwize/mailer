acea579c0f34fea524334585f3c23143d721b46f
NakshatraCodes <nakshatra163@gmail.com>
2021-02-28 03:22:24 +0530

    Auth Setup Done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,29 @@
-const passport = require('passport');
+const passport = require("passport");
 
 module.exports = (app) => {
+  // Client requests OAuth from Google
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
 
-    // Client requests OAuth from Google
-    app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
-    }));
+  // Handles Redirect from Google Servers
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
-    // Handles Redirect from Google Servers
-    app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 
-    app.get('/api/logout', (req, res) => {
-        req.logout();
-        res.send({ message: "You were LOGGED OUT" });
-    });
-
-    app.get('/api/current_user', (req, res) => {
-        res.send(req.user);
-    });
-}
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+};
