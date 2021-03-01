4a432c9a09c58c9080296e44230fd353a998e4d2
NakshatraCodes <nakshatra163@gmail.com>
2021-03-01 22:43:14 +0530

    Production Tweaks

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const keys = require('./config/keys');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
-require('./Models/User');
-require('./services/passport');
+require("./Models/User");
+require("./services/passport");
 
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(
+  keys.mongoURI,
+  {
+    authSource: "admin",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to DB Servers");
+  }
+);
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(keys.mongoURI, {
-        authSource: 'admin',
-        useNewUrlParser: true ,
-        useUnifiedTopology: true
-    }, () => {
-        console.log("Connected to DB Servers");
-    });
-
-app.use(cookieSession({
+app.use(
+  cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-}));
+    keys: [keys.cookieKey],
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-(require('./routes/authRoutes'))(app);
+require("./routes/authRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Serving Production assets
+  app.use(express.static("client/build"));
+
+  // If it doesnt recognize the route it will serve index.html
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 9000;
-app.listen(PORT, ()=>{
-    console.log(`Server on: http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server on: http://localhost:${PORT}`);
 });
