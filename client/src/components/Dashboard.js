fe58870996c4187da522dae03921f4fffaa963f2
NakshatraCodes <nakshatra163@gmail.com>
2021-03-05 23:38:13 +0530

    Create Dash

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h5>Dashboard</h5>
+      <div className="fixed-action-btn">
+        <Link className="btn-floating btn-large red" to="/surveys/new">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
     </div>
   );
 };
